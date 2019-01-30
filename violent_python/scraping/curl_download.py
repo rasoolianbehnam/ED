4b5581cbbcce6af1358901b8e6a2d19567b4fc62
rasoolianbehnam <behnam.rasoulian@gmail.com>
2019-01-30 10:54:17 -0600

    save

--- violent_python/scraping/curl_download.py
+++ violent_python/scraping/curl_download.py
@@ -6,31 +6,21 @@ import os
 import time
 
 parallel_requests = 8
-curl = "curl 'https://s20.7fuc.xyz/hls/qvsbcngfn7blgwsztrtka66tjmpr76uxm242ukcoqcyzewtljstdxp6tjuga/seg-6290-v1-a1.jpg' -H 'origin: https://putlocker9.nl' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9' -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36 OPR/56.0.3051.99' -H 'accept: */*' -H 'referer: https://putlocker9.nl/film/the-predator-2018-1080p.64463/watching.html' -H 'authority: s20.7fuc.xyz' --compressed"
-max_style = False
+curl_template = "curl 'https://stream-2-1.loadshare.org/stream/VideoID-DoIf7G1b/lK-SZ8QcWSwVv07elGprae7L35s83JC3oK-_i5Iy80Ye0_XPzAG7Qa_avoWuDCRGE7SxxPdT2vOJplckgQ9ixen5ckjhtWT2hXlz_OJbxbv6ZnOjcPpHQcnC0tjY_p92Xk0Xa97hDahkvyL1OH4nRA/seg-%d-f2-v1-a1.ts?token=ip=97.80.236.60~st=1548823923~exp=1548838323~acl=/*~hmac=ce9a2bbc047fc442345a99c0b94d14dae407662630cab5c03ce068b4ac244e13' -H 'origin: https://putlocker.digital' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9' -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 OPR/57.0.3098.116' -H 'accept: */*' -H 'referer: https://putlocker.digital/tv-series/mindhunter-season-1/QKfGQ5SX/l3ADtrHL/oZOmToTI-watch-online-free.html' -H 'authority: stream-2-1.loadshare.org' --compressed"
+max_num = 615
 
-curl = curl.replace('%', '%%')
 url_regex = re.compile(r'\'.*?\'')
-url = url_regex.search(curl).group(0)[1:-1]
-#print url
-file_name = url.split('/')[-1]
-#print file_name
-max_num = file_name.split('-')[1].split('.')[0]
-len_max_num = len(max_num)
-print max_num, len_max_num
+url = url_regex.search(curl_template).group(0)[1:-1]
+print "url = ", url
+indexOfParams = url.find("?")
+if indexOfParams > 0:
+    url = url[:indexOfParams]
 
-if max_style:
-    file_name_template = file_name.replace(max_num, '%%0%dd'%len_max_num)
-    print file_name_template
+file_name_template = url.split('/')[-1]
+print "file name: ", file_name_template
 
-    curl_template = curl.replace(max_num, '%%0%dd'%len_max_num)
-    print curl_template
-else:
-    file_name_template = file_name.replace(max_num, '%d')
-    print file_name_template
-    curl_template = curl.replace(max_num, '%d')
-    print curl_template
-
+print("url template: %s"%(url))
+print("file name: %s"%(file_name_template))
 
 q = Queue.Queue()
 counts = {}
--- violent_python/scraping/curl_download.scala
+++ violent_python/scraping/curl_download.scala
@@ -2,8 +2,9 @@ import scala.collection.mutable._
 import scala.sys.process._
 import java.io.File
 
+object curl_download extends App{
 /* User provided */
-var curl_original = """curl 'https://s20.7fuc.xyz/hls/qvsbcngfn7blgwsztrtka66tjmpr76uxm242ukcoqcyzewtljstdxp6tjuga/seg-%s-v1-a1.jpg' -H 'origin: https://putlocker9.nl' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9' -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36 OPR/56.0.3051.99' -H 'accept: */*' -H 'referer: https://putlocker9.nl/film/the-predator-2018-1080p.64463/watching.html' -H 'authority: s20.7fuc.xyz' --compressed -O 2> /dev/null""" 
+var curl_original = """curl 'https://s29.openstream.io/hls/qvsbdvflndblgwsztqekayofkc3dkdpuhxce4tsdjtjuj7vrh6rwq42agxlq/seg-%s-v1-a1.jpg' -H 'Origin: https://embed.streamx.me' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 OPR/57.0.3098.116' -H 'Accept: */*' -H 'Referer: https://embed.streamx.me/?k=3da769b6aeb4a0d043107299e356304a&li=0&tham=1548810901&lt=os&st=5b59af664b9f90b4d00f73cc3916bb6639f1887f6906d8baf42f1af02624db0767400d97a877adfc3bc0d69588e08074e5198ee6e55a35e570918bf45efb4a4243401226356cf12d109ca4d729395ebf263a99bc8bdd5975df50115bfb316fe79929358b23838c2a0b2f132e443d42e4973dc0d6f17889e485427583e7ad2139&qlt=720p&spq=p&prv=&key=38e521c260b83b0b7b9731cae1be3257&h=1548810901&w=100%&h=675' -H 'Connection: keep-alive' --compressed -O 2> /dev/null""" 
 curl_original = "echo " + curl_original
 val numberFormat = "%d"
 val maxNumber = 3
@@ -37,3 +38,4 @@ def block() {
 
 val threads = for (i <- 1 to numThreads) yield createThread(block)
 for (thread <- threads) thread.start()
+}
