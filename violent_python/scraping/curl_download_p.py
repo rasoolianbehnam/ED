f9fb4602834c1174aa02e6976bab366d6237699f
rasoolianbehnam <behnam.rasoulian@gmail.com>
2019-01-30 23:18:54 -0600

    save

--- violent_python/scraping/curl_download_p.py
+++ violent_python/scraping/curl_download_p.py
@@ -1,14 +1,14 @@
 import re
 import Queue
 import subprocess
-import threading
+import multiprocessing
 import os
 import time
 
 parallel_requests = 8
 curl_template = "curl 'https://stream-2-1.loadshare.org/stream/VideoID-1TH7WFHD/9mBPaYgHDTRXpECnTJpN5Ed7XmiG1yPmu7Em98dP7B9rSs9NpRP3BmGfEcmOG6NNcp_TtqsTDwqA91n8jijZuak-vw2tbrNwZeeytk3924oyRVtQGBKApJEWse8mB2QENAY6QY9rzS3dpz1BP9tO0w/seg-%d-f2-v1-a1.ts?token=ip=97.80.236.60~st=1548905759~exp=1548920159~acl=/*~hmac=b258f52f550178727e5ef807054c0ffe40611ac26bee6a0839f9970b6f83c3a5' -H 'origin: https://putlocker.digital' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9' -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 OPR/57.0.3098.116' -H 'accept: */*' -H 'referer: https://putlocker.digital/tv-series/stranger-things-season-1/7JacmszI/SkZZQhn5/MsfycXmr-watch-online-free.html' -H 'authority: stream-2-1.loadshare.org' --compressed"
-min_num = 1
-max_num = 100
+min_num = 101
+max_num = 623
 
 url_regex = re.compile(r'\'.*?\'')
 url = url_regex.search(curl_template).group(0)[1:-1]
@@ -23,7 +23,7 @@ print "file name: ", file_name_template
 print("url template: %s"%(url))
 print("file name: %s"%(file_name_template))
 
-q = Queue.Queue()
+q = multiprocessing.Queue()
 counts = {}
 max_tries = 5
 for i in range(min_num, max_num+1):
@@ -55,7 +55,7 @@ def test():
 
 
 for i in range(parallel_requests):
-    my_thread = threading.Thread(target=test)
+    my_thread = multiprocessing.Process(target=test)
     my_thread.start()
     #test()
 
