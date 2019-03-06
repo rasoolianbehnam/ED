2715044fb664626784e2d4cfda14f239ac7890cc
rasoolianbehnam <behnam.rasoulian@gmail.com>
2019-03-06 17:31:02 -0600

    save

--- drafts/python/wxpython/launch/show_images.py
+++ drafts/python/wxpython/launch/show_images.py
@@ -1,3 +1,17 @@
+import os
+try:
+    import wx
+    print("wx is installed")
+except:
+    os.system('pip install wxpython')
+
+try:
+    import cv2
+    print("cv2 is installed")
+except:
+    os.system('pip install opencv-python')
+
+
 import wx
 import cv2
 
