58c82bd78f437d516ee79eada1261fc1ac17d530
rasoolianbehnam <behnam.rasoulian@gmail.com>
2019-09-08 11:50:46 -0500

    save

---
+++ data_mining/utilities.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import collections
+from scipy.stats import pearsonr
+
+
+def calculate_pvalues(df):
+    df = df.dropna()._get_numeric_data()
+    dfcols = pd.DataFrame(columns=df.columns)
+    pvalues = dfcols.transpose().join(dfcols, how='outer')
+    for r in df.columns:
+        for c in df.columns:
+            pvalues[r][c] = round(pearsonr(df[r], df[c])[1], 4)
+    return pvalues
